a960c24a0673477df8bc1950c7e5d37337ec0682
Sanjay kumar <sanjayocsm85@gmail.com>
2022-12-01 10:15:09 +0530

    MongoClint

---
+++ MongoDBClient/index.js
@@ -0,0 +1,194 @@
+const {MongoClient} = require("mongodb");
+//giving the address of database
+const url ="mongodb://127.0.0.1:27017/"//this is server where my database will be run
+const client = new MongoClient(url);
+//for connecting our database
+async function Createdb(){
+    await client.connect();
+    console.log("client connected...");
+    //DataBase created...
+    const dataBaseName = client.db("Human_Resources");
+
+    //collection created...
+    const collectionName = dataBaseName.collection("employee");
+
+    const employeeData = (
+        [{
+  
+            "firstName": "John",
+            "lastName": "Doe",
+            "salary": "25000",
+            "department": "HR",
+            "lastCompany": "X",
+            "lastSalary": "10000",
+            "overallExp": "2",
+            "contactInfo": "1234567890",
+            "yearGrad": "2016",
+            "gradStream": "CSE"
+          },{
+            "firstName": "Rohan",
+            "lastName": "Jame",
+            "salary": "30000",
+            "department": "Technical",
+            "lastCompany": "Y",
+            "lastSalary": "15000",
+            "overallExp": "1",
+            "contactInfo": "1234567860",
+            "yearGrad": "2015",
+            "gradStream": "CSE"
+          },{
+            "firstName": "Jame",
+            "lastName": "Doe",
+            "salary": "35000",
+            "department": "Accounts",
+            "lastCompany": "Z",
+            "lastSalary": "20000",
+            "overallExp": "1",
+            "contactInfo": "123567890",
+            "yearGrad": "2019",
+            "gradStream": "ECE"
+          },{
+            
+            "firstName": "Sao",
+            "lastName": "Avika",
+            "salary": "30000",
+            "department": "Sales",
+            "lastCompany": "Y",
+            "lastSalary": "15000",
+            "overallExp": "2",
+            "contactInfo": "1234567860",
+            "yearGrad": "2015",
+            "gradStream": "CSE"
+          },{
+            "firstName": "Jame",
+            "lastName": "roh",
+            "salary": "35000",
+            "department": "Accounts",
+            "lastCompany": "Z",
+            "lastSalary": "15000",
+            "overallExp": "2",
+            "contactInfo": "123567890",
+            "yearGrad": "2019",
+            "gradStream": "EEE"
+          },{
+            
+            "firstName": "Rohan",
+            "lastName": "Jame",
+            "salary": "30000",
+            "department": "Technical",
+            "lastCompany": "Y",
+            "lastSalary": "15000",
+            "overallExp": "1",
+            "contactInfo": "1234567860",
+            "yearGrad": "2015",
+            "gradStream": "CSE"
+          },{
+            
+            "firstName": "Jame",
+            "lastName": "Doe",
+            "salary": "35000",
+            "department": "Accounts",
+            "lastCompany": "Z",
+            "lastSalary": "20000",
+            "overallExp": "1",
+            "contactInfo": "123567890",
+            "yearGrad": "2019",
+            "gradStream": "ECE"
+          },{
+            
+            "firstName": "Sao",
+            "lastName": "Avika",
+            "salary": "30000",
+            "department": "Sales",
+            "lastCompany": "Y",
+            "lastSalary": "15000",
+            "overallExp": "2",
+            "contactInfo": "1234567860",
+            "yearGrad": "2015",
+            "gradStream": "CSE"
+          },{
+            
+            "firstName": "Jame",
+            "lastName": "Doe",
+            "salary": "35000",
+            "department": "Accounts",
+            "lastCompany": "Z",
+            "lastSalary": "15000",
+            "overallExp": "2",
+            "contactInfo": "123567890",
+            "yearGrad": "2019",
+            "gradStream": "EEE"
+          },{
+          
+            "firstName": "Rohan",
+            "lastName": "Jame",
+            "salary": "30000",
+            "department": "Technical",
+            "lastCompany": "Y",
+            "lastSalary": "15000",
+            "overallExp": "1",
+            "contactInfo": "1234567860",
+            "yearGrad": "2015",
+            "gradStream": "CSE"
+          },{
+            
+            "firstName": "Jame",
+            "lastName": "Doe",
+            "salary": "35000",
+            "department": "Accounts",
+            "lastCompany": "Z",
+            "lastSalary": "20000",
+            "overallExp": "1",
+            "contactInfo": "123567890",
+            "yearGrad": "2019",
+            "gradStream": "ECE"
+          },{
+            
+            "firstName": "Sao",
+            "lastName": "Avika",
+            "salary": "30000",
+            "department": "Sales",
+            "lastCompany": "Y",
+            "lastSalary": "15000",
+            "overallExp": "2",
+            "contactInfo": "1234567860",
+            "yearGrad": "2015",
+            "gradStream": "CSE"
+          },{
+            
+            "firstName": "Jame",
+            "lastName": "Doe",
+            "salary": "35000",
+            "department": "Accounts",
+            "lastCompany": "Z",
+            "lastSalary": "15000",
+            "overallExp": "2",
+            "contactInfo": "123567890",
+            "yearGrad": "2019",
+            "gradStream": "EEE"
+          }]
+    );
+    const response = await collectionName.insertMany(employeeData);
+    console.log("responses are...",response);
+
+    const dataFind = await collectionName.find().toArray();
+    console.log("find all data",dataFind);
+
+    const findSalary = await collectionName.find({"salary" : {$gt:"30000"}}).toArray();
+    console.log("finding salary",findSalary);
+
+    const findExp = await collectionName.find({"overallExp" : {$gt:"2"}}).toArray();
+    console.log("overall experience",findExp);
+
+    const findGradExp = await collectionName.find({yearGrad:{$gt:"2015"},overallExp:{$gt:"1"}}).toArray();
+    console.log(findGradExp);
+
+    const updateEmp = await collectionName.updateMany({"salary":{$gt:"70000"}},{$set:{"salary":"65000"}});
+    console.log(updateEmp);
+
+    const deleteLast = await collectionName.deleteMany({"lastCompany":"Y"});
+    console.log(deleteLast);
+
+}
+
+Createdb();
